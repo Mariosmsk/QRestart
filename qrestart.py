e12d9816e6ac0a90c35c27cb540096ae4f4b8aa2
Marios S. Kyriakou <mariosmsk@gmail.com>
2026-01-16 21:30:13 +0200

    Add Qt6 support and improve QGIS restart logic

--- qrestart.py
+++ qrestart.py
@@ -23,71 +23,46 @@
  *                                                                         *
  ***************************************************************************/
 """
-from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication
+
+from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication, QProcess
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtWidgets import QAction, QMessageBox
 
-# Initialize Qt resources from file resources.py
-from .resources import *
 from qgis.core import QgsApplication, QgsProject
+
 import os.path
-import subprocess
+
+try:
+    from . import resources  # noqa: F401
+except Exception:
+    resources = None
 
 
 class QRestart:
     """QGIS Plugin Implementation."""
 
     def __init__(self, iface):
-        """Constructor.
-
-        :param iface: An interface instance that will be passed to this class
-            which provides the hook by which you can manipulate the QGIS
-            application at run time.
-        :type iface: QgsInterface
-        """
-        # Save reference to the QGIS interface
         self.iface = iface
-        # initialize plugin directory
         self.plugin_dir = os.path.dirname(__file__)
-        # initialize locale
-        locale = QSettings().value('locale/userLocale')[0:2]
-        locale_path = os.path.join(
-            self.plugin_dir,
-            'i18n',
-            'QRestart_{}.qm'.format(locale))
+
+        locale = (QSettings().value("locale/userLocale") or "en")[0:2]
+        locale_path = os.path.join(self.plugin_dir, "i18n", f"QRestart_{locale}.qm")
 
         if os.path.exists(locale_path):
             self.translator = QTranslator()
             self.translator.load(locale_path)
             QCoreApplication.installTranslator(self.translator)
 
-        # Declare instance attributes
         self.actions = []
-        self.menu = self.tr(u'&QRestart')
+        self.menu = self.tr("&QRestart")
 
-        # Check if plugin was started the first time in current QGIS session
-        # Must be set in initGui() to survive plugin reloads
-        self.toolbar = self.iface.addToolBar(u'QRestart')
-        self.toolbar.setObjectName(u'QRestart')
-        self.iface.addToolBar(self.toolbar)
+        self.toolbar = self.iface.addToolBar("QRestart")
+        self.toolbar.setObjectName("QRestart")
 
         self.first_start = None
 
-    # noinspection PyMethodMayBeStatic
     def tr(self, message):
-        """Get the translation for a string using Qt translation API.
-
-        We implement this ourselves since we do not inherit QObject.
-
-        :param message: String for translation.
-        :type message: str, QString
-
-        :returns: Translated version of message.
-        :rtype: QString
-        """
-        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
-        return QCoreApplication.translate('QRestart', message)
-
+        return QCoreApplication.translate("QRestart", message)
 
     def add_action(
         self,
@@ -99,46 +74,8 @@ class QRestart:
         add_to_toolbar=True,
         status_tip=None,
         whats_this=None,
-        parent=None):
-        """Add a toolbar icon to the toolbar.
-
-        :param icon_path: Path to the icon for this action. Can be a resource
-            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
-        :type icon_path: str
-
-        :param text: Text that should be shown in menu items for this action.
-        :type text: str
-
-        :param callback: Function to be called when the action is triggered.
-        :type callback: function
-
-        :param enabled_flag: A flag indicating if the action should be enabled
-            by default. Defaults to True.
-        :type enabled_flag: bool
-
-        :param add_to_menu: Flag indicating whether the action should also
-            be added to the menu. Defaults to True.
-        :type add_to_menu: bool
-
-        :param add_to_toolbar: Flag indicating whether the action should also
-            be added to the toolbar. Defaults to True.
-        :type add_to_toolbar: bool
-
-        :param status_tip: Optional text to show in a popup when mouse pointer
-            hovers over the action.
-        :type status_tip: str
-
-        :param parent: Parent widget for the new action. Defaults None.
-        :type parent: QWidget
-
-        :param whats_this: Optional text to show in the status bar when the
-            mouse pointer hovers over the action.
-
-        :returns: The action that was created. Note that the action is also
-            added to self.actions list.
-        :rtype: QAction
-        """
-
+        parent=None,
+    ):
         icon = QIcon(icon_path)
         action = QAction(icon, text, parent)
         action.triggered.connect(callback)
@@ -151,68 +88,79 @@ class QRestart:
             action.setWhatsThis(whats_this)
 
         if add_to_toolbar:
-            # Adds plugin icon to Plugins toolbar
             self.toolbar.addAction(action)
 
         if add_to_menu:
-            self.iface.projectMenu().addAction(action)
-            #
-            # self.iface.addPluginToMenu(
-            #     self.menu,
-            #     action)
+            self.iface.addPluginToMenu(self.menu, action)
 
         self.actions.append(action)
-
         return action
 
     def initGui(self):
-        """Create the menu entries and toolbar icons inside the QGIS GUI."""
-
-        icon_path = ':/plugins/qrestart/icon.png'
+        icon_path = ":/plugins/qrestart/icon.png"
         self.add_action(
             icon_path,
-            text=self.tr(u'Restart QGIS'),
+            text=self.tr("Restart QGIS"),
             callback=self.run,
-            parent=self.iface.mainWindow())
-
-        # will be set False in run()
+            parent=self.iface.mainWindow(),
+        )
         self.first_start = True
 
-
     def unload(self):
-        """Removes the plugin menu item and icon from QGIS GUI."""
         for action in self.actions:
             try:
-                self.iface.removePluginMenu(
-                self.tr(u'&QRestart'),
-                action)
-
-                self.iface.removeToolBarIcon(action)
-            except:
+                self.iface.removePluginMenu(self.menu, action)
+            except Exception:
                 pass
             try:
-                self.iface.projectMenu().removeAction(action)
-            except:
+                self.toolbar.removeAction(action)
+            except Exception:
                 pass
 
+    def _ask_to_save_if_dirty(self):
+        if not QgsProject.instance().isDirty():
+            return True
+
+        msg_box = QMessageBox(self.iface.mainWindow())
+        msg_box.setWindowTitle(self.tr("Save project"))
+        msg_box.setText(self.tr("Do you want to save the changes to the project?"))
+
+        # Qt6 uses QMessageBox.StandardButton enum (Save/Discard/Cancel are not attributes on QMessageBox)
+        buttons = (
+            QMessageBox.StandardButton.Save
+            | QMessageBox.StandardButton.Discard
+            | QMessageBox.StandardButton.Cancel
+        )
+        msg_box.setStandardButtons(buttons)
+        msg_box.setDefaultButton(QMessageBox.StandardButton.Save)
+        msg_box.setIcon(QMessageBox.Icon.Information)
+
+        exec_fn = getattr(msg_box, "exec", None) or getattr(msg_box, "exec_", None)
+        clicked = exec_fn()
+
+        # clicked can be int (Qt5) or StandardButton (Qt6). Normalize.
+        if int(clicked) == int(QMessageBox.StandardButton.Save):
+            self.iface.actionSaveProject().trigger()
+            return True
+
+        if int(clicked) == int(QMessageBox.StandardButton.Discard):
+            QgsProject.instance().clear()
+            return True
+
+        return False
+
     def run(self):
-        """Run method that performs all the real work"""
-        if QgsProject.instance().isDirty():
-            msg_box = QMessageBox()
-            msg_box.setWindowTitle("Save project")
-            msg_box.setText("Do you want to save the changes to the project?")
-            msg_box.setStandardButtons(QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel)
-            msg_box.setDefaultButton(QMessageBox.Save)
-            msg_box.setIcon(QMessageBox.Information)
-            button_clicked = msg_box.exec_()
-            if button_clicked == QMessageBox.Save:
-                self.iface.actionSaveProject().trigger()
-            elif button_clicked == QMessageBox.Discard:
-                QgsProject.instance().clear()
-            else:
-                return
+        if not self._ask_to_save_if_dirty():
+            return
 
+        qgis_exe = QgsApplication.applicationFilePath()
+        if not qgis_exe:
+            QMessageBox.warning(
+                self.iface.mainWindow(),
+                self.tr("Restart QGIS"),
+                self.tr("Could not determine the QGIS executable path."),
+            )
+            return
+
+        QProcess.startDetached(qgis_exe, [])
         self.iface.actionExit().trigger()
-        subprocess.Popen(QgsApplication.applicationFilePath())
-
-
